1fc6087f65007e88af3c8e134edd117345764235
Royaltechsis <osenibunmi2023@gmail.com>
2024-09-19 00:39:55 +0100

    cutomzation 1 update

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,9 @@ import './index.css'
 
 
 const DATA = [
-  { id: "todo-0", name: "Eat", completed: true },
-  { id: "todo-1", name: "Sleep", completed: false },
-  { id: "todo-2", name: "Repeat", completed: false },
+  { id: "todo-0", name: "Learn React", completed: true },
+  { id: "todo-1", name: "Become a cool Developer", completed: false },
+  { id: "todo-2", name: "Work on cool projects", completed: false },
 ];
 
 
